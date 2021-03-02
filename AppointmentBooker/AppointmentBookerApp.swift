46c3962ef3987fb9384f69be2678f1619e621b91
Yeseo Kim <YKim@lcs.on.ca>
2021-03-02 14:19:16 -0500

    Initial Commit

---
+++ AppointmentBooker/AppointmentBookerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppointmentBookerApp.swift
+//  AppointmentBooker
+//
+//  Created by Yeseo Kim on 2021-03-02.
+//
+
+import SwiftUI
+
+@main
+struct AppointmentBookerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
